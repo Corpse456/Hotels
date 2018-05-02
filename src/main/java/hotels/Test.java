4f62878f9747b22ca28ff366e2bb2802fe7c6897
Alexander Neznaev <Corpsetrup@gmail.com>
2018-05-02 21:38:06 +0200

    some progress

--- src/main/java/hotels/Test.java
+++ src/main/java/hotels/Test.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import hotels.categoryUI.Category;
+import hotels.holelsUI.Hotel;
 
 public class Test {
     public static void main (String[] args) {
@@ -15,10 +16,15 @@ public class Test {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         
-        Category categ = new Category("MyExperiment2");
+        Category categ = new Category("Binded14");
+        Hotel hotel = new Hotel("NewHib", "InMysql", 5, 4354L, categ, "dsd", "Empty as usual");
         
         em.persist(categ);
+        
+        em.persist(hotel);
+        
         tx.commit();
         em.close();
+        emf.close();
     }
 }
--- src/main/java/hotels/holelsUI/Hotel.java
+++ src/main/java/hotels/holelsUI/Hotel.java
@@ -2,18 +2,21 @@ package hotels.holelsUI;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import hotels.categoryUI.Category;
 
 @Entity
-@SuppressWarnings("serial")
+@SuppressWarnings ("serial")
 public class Hotel implements Serializable, Cloneable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name = "";
@@ -22,107 +25,110 @@ public class Hotel implements Serializable, Cloneable {
 
     private Integer rating;
 
+    @Column (name = "OPERATES_FROM")
     private Long operatesFrom;
 
+    @JoinColumn (name = "CATEGORY_ID")
+    @ManyToOne
     private Category category;
 
     private String url;
-    
+
     private String description;
 
-    public boolean isPersisted() {
+    public boolean isPersisted () {
         return id != null;
     }
 
     @Override
-    public String toString() {
+    public String toString () {
         return name + " " + rating + "stars " + address;
     }
 
     @Override
-    protected Hotel clone() throws CloneNotSupportedException {
+    protected Hotel clone () throws CloneNotSupportedException {
         return (Hotel) super.clone();
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals (Object obj) {
         return super.equals(obj);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode () {
         return super.hashCode();
     }
 
-    public Hotel() {
+    public Hotel () {
     }
 
-    public Long getId() {
+    public Long getId () {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId (Long id) {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName () {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName (String name) {
         this.name = name;
     }
 
-    public String getAddress() {
+    public String getAddress () {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress (String address) {
         this.address = address;
     }
 
-    public Integer getRating() {
+    public Integer getRating () {
         return rating;
     }
 
-    public void setRating(Integer rating) {
+    public void setRating (Integer rating) {
         this.rating = rating;
     }
 
-    public Long getOperatesFrom() {
+    public Long getOperatesFrom () {
         return operatesFrom;
     }
 
-    public void setOperatesFrom(Long operatesFrom) {
+    public void setOperatesFrom (Long operatesFrom) {
         this.operatesFrom = operatesFrom;
     }
 
-    public Category getCategory() {
+    public Category getCategory () {
         return category;
     }
 
-    public void setCategory(Category category) {
+    public void setCategory (Category category) {
         this.category = category;
     }
 
-    public String getUrl() {
+    public String getUrl () {
         return url;
     }
 
-    public void setUrl(String url) {
+    public void setUrl (String url) {
         this.url = url;
     }
-    
-    public String getDescription() {
+
+    public String getDescription () {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription (String description) {
         this.description = description;
     }
 
-    public Hotel(String name, String address, Integer rating, Long operatesFrom, Category category,
-            String url, String description) {
+    public Hotel (String name, String address, Integer rating, Long operatesFrom, Category category, String url,
+            String description) {
         super();
         this.name = name;
         this.address = address;
